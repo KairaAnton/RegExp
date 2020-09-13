6161630d7c891cba419bea0a6b88dfb73f0ce56e
KairaAnton <semeshko.94@gmail.com>
2020-09-13 18:20:45 +0300

    finished task

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,7 @@ const regexp = new RegExp('^([a-z]{8,12})$',);
 input.addEventListener("keypress", (event) => {
     const str = event.target.value;
     console.log(regexp.test(str));
-    if(regexp.test(str) === true){form.className = "formGreen"};
-    if(regexp.test(str) === false){form.className = "formRed"};
+    if(regexp.test(str) === false){form.className = "formRed";
+}else{form.className = "formGreen"};
+
 })
