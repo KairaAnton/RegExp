d11f65533ae3f7cbb77bc49c6d2fc75d059783f9
KairaAnton <semeshko.94@gmail.com>
2020-09-16 15:39:59 +0300

    correction by comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,6 @@ const regexp = new RegExp('^([a-z]{8,12})$',);
 input.addEventListener("input", (event) => {
     const { target:
         { value } } = event;
-
-    console.log(regexp.test(value));
-
     if (regexp.test(value) === true) {
         form.className = "formGreen";
     } else { form.className = "formRed" };
